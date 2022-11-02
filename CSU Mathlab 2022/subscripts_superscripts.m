c61ba376f057fbc17dcd62806c0436ebaf86e728
JerryZ1 <116749171+JerryZ1@users.noreply.github.com>
2022-11-02 13:25:03 -0400

    Add files via upload

---
+++ CSU Mathlab 2022/subscripts_superscripts.m
@@ -0,0 +1,7 @@
+x=linspace(0,2*pi);
+y1=cos(x);
+y2=sin(x);
+plot(x,y1,'r',x,y2,'b','linewidth',2)
+text(3,.6,'\alpha_z+ \beta^2+ a_3^4','fontSize',14,'color','b');
+
+title('Adding text with Gerrk letters, Subscripts and Super scripts')
